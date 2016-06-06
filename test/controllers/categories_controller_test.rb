92ba82018e423db0862ec3932211f4c3d53182d5
ElsaLau@cloud9 <chinbo@gmail.com>
2016-06-06 12:12:50 +0000

    Add require admin user for create action and update navbar for categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
   def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
     @category = Category.create(name: "sports")
   end
   
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -18,5 +20,12 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get( :show, {'id' => @category.id })
     assert_response :success
+  end
+  
+  test "should redirect create when admin not login" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
   end
 end
